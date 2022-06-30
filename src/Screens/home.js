9f7db111254f7a9a0183c0cf7bd4f8b14d137ad4
RobertDjukic <rdjuk093@uottawa.ca>
2022-06-30 11:42:47 -0400

    changed router

--- src/Screens/home.js
+++ src/Screens/home.js
@@ -12,30 +12,30 @@ export default function AboutScreen() {
             <section className="bg-light">
                 <div className="container-fluid">
                     <Image src={HeaderImage} className="img-fluid rounded mx-auto d-block" alt="Responsive image"/>
-                    <mark className="display-1 text-dark bg-light" style={{position:'absolute', top: '50%', left: '50%', transform: 'Translate(-50%,-50%)'}}>Join The Club Today</mark>
+                    <mark className="display-1 text-dark bg-light" style={{position:'absolute', top: '50%', left: '50%', transform: 'Translate(-50%,-50%)'}}>Robert's Waterpolo</mark>
                 </div>
                 <section class="ms-5 me-5 mt-2">
                     <div class="container-fluid">
                         <div className="row">
                             <div class="col p-3  bg-white text-center">
-                                <Image src={program} className="img-fluid rounded mx-auto d-block" alt="Responsive image"/>
+                                <Image src={program} className="img-fluid rounded mx-auto d-block" alt="Responsive image" style={{height: '300px'}}/>
                                 <h3>Programs:</h3>
                                 <h3 className="text-muted">See what program is perfect for you!</h3>
                             </div>
                             <div class="col p-3 ms-3 bg-white text-center">
-                                <Image src={coach} className="img-fluid rounded mx-auto d-block" alt="Responsive image"/>
+                                <Image src={coach} className="img-fluid rounded mx-auto d-block" alt="Responsive image" style={{height: '300px'}}/>
                                 <h3>About:</h3>
-                                <h3 className="text-muted">Learn more about our </h3>
+                                <h3 className="text-muted">Learn more about our club</h3>
                             </div>
                         </div>
                         <div className="row mt-5">
                             <div class="col p-3  bg-white text-center">
-                                <Image src={faq} className="img-fluid rounded mx-auto d-block" alt="Responsive image"/>
+                                <Image src={faq} className="img-fluid rounded mx-auto d-block" alt="Responsive image" style={{height: '300px'}}/>
                                 <h3>FAQ:</h3>
                                 <h3 className="text-muted">Have questions or comments? Find the answers here</h3>
                             </div>
                             <div class="col p-3 ms-3 bg-white text-center">
-                                <Image src={register} className="img-fluid rounded mx-auto d-block" alt="Responsive image"/>
+                                <Image src={register} className="img-fluid rounded mx-auto d-block" alt="Responsive image" style={{height: '300px'}}/>
                                 <h3>Regostration:</h3>
                                 <h3 className="text-muted">Register for the program of your choice by following a few easy steps</h3>
                             </div>
